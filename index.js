1f5d146dd216071b2a7e1bd7dbda63455b8324de
Alexsandro Vítor <avsc@cin.ufpe.br>
2018-06-03 00:10:41 -0300

    Adição de legendas e conserto da codificação

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // SVG
 var svgTag = d3.select("body").append("svg")
-	.attr("width", 800)
-	.attr("height", 400);
+	.attr("width", 1200)
+	.attr("height", 600);
 	
 // Mapa
 var mapa = new Map(svgTag, "mapa", null, 2, null);
@@ -10,19 +10,25 @@ var mapa = new Map(svgTag, "mapa", null, 2, null);
 d3.json("custom.geojson", function(error, geoData) {
 	console.log(geoData);
 	
-	mapa.fillValue((d, i)=>0);
-	mapa.projection(d3.geoNaturalEarth1());
-	mapa.setMap(geoData, {id: (d, i)=>d.properties.name});
+	mapa.fillValue((d, i)=> d ? 0 : 1)
+		.projection(d3.geoNaturalEarth1())
+		.setMap(geoData, {id: (d, i)=>d.properties.name})
+		.labelTable(new LabelTable(mapa, "legenda", {x: 0, y: 450}, 0, {width: 340, height: 80}));
+	
+	mapa.labelTable()
+		.setValues([0, 1].map((d, i) => mapa.colorScheme()(i)), 
+				["Temos dados", "NÃ£o temos dados"],
+				null,
+				{"font-size": "40"});
 	
 	// Csv
 	d3.csv("filteredData.csv", function(erro, csvData) {
 		let set = new Set(csvData.map(d=>d['Region']));
 		
-		// Atualmente, o mapa mostra apenas os países dos quais temos dados
-		// Obs.: Não sei exatamente o que são as siglas de "Region", mas elas coincidiram com 51 das siglas em iso_a2, então estou usando isso
+		// Atualmente, o mapa mostra apenas os paÃ­ses dos quais temos dados
+		// Obs.: NÃ£o sei exatamente o que sÃ£o as siglas de "Region", mas elas coincidiram com 51 das siglas em iso_a2, entÃ£o estou usando isso
 		let data = geoData.features.map(d=>set.has(d.properties.iso_a2.toLowerCase()));
-		// Preto == Temos dados, Bege: Não temos
-		mapa.fillValue((d, i)=> d ? 0 : 1);
+		// Preto == Temos dados, Bege: NÃ£o temos
 		mapa.setData(data, null, null);
 	});
 });
